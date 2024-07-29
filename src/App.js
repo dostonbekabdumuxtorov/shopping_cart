441bbaac96b7e95992c6f0eb8a8276cd4d8719f9
dostonbekabdumuxtorov <simple2000code@gmail.com>
2024-07-30 02:16:25 +0500

    readme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
-import Products from './pages/Products/Products'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Cart />} />
-        <Route path='/products' element={<Products />} />
       </Routes>
     </BrowserRouter>
   )
